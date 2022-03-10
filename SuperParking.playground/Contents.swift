c55384261fa9db52d627aaea2fe0a686799ff164
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-03-10 19:00:43 -0300

    Exercise 4
    Added parkedTime propertie

--- SuperParking.playground/Contents.swift
+++ SuperParking.playground/Contents.swift
@@ -6,31 +6,32 @@ import UIKit
 // As Vehicle implements Parkable protocol, the properties must be added in both
 // MARK: Answer exercise 3.2:
 // All data types can be optional
+// MARK: Answer exercise 4:
+// Computed properties have the ability to be computed each time they are queried
 protocol Parkable {
     var plate: String { get }
     var type: VehicleType { get  }
     var checkInTime: Date { get }
     var discountCard: String? { get }
+    var parkedTime: Calendar { get }
 }
 
 
 // MARK: struct Parking - represents the parking
+// Property vehicles - it stores parked vehicles
+// MARK: Aswer exercise 1:
+// Why is it defined as a set? -> because it doesn't allow repeating values
 struct Parking {
-    // Property vehicles - it stores parked vehicles
-    // MARK: Aswer exercise 1:
-    // Why is it defined as a set? -> because it doesn't allow repeating values
-    
     var vehicles: Set<Vehicle> = []
 }
 
 // MARK: Struct Vehicle - implements Hashable protocol, used to uniquely identify elements.
+//    MARK: Answer exercise 2.2:
+//    type is a constant because Vehicle can't changes his VehicleType
 struct Vehicle: Parkable, Hashable {
+    var parkedTime: Calendar
     var discountCard: String?
-    
     var checkInTime: Date
-    
-    //    MARK: Answer exercise 2.2:
-    //    type is a constant because Vehicle can't changes his VehicleType
     let type: VehicleType
     let plate: String // Corresponds to the license plate of the vehicle.
     
@@ -43,11 +44,11 @@ struct Vehicle: Parkable, Hashable {
 }
 
 // MARK: enum VehicleType - determines vehicle type and initial fee
+//        Switch is used for flow control because it evaluates each case of the enum.
 enum VehicleType {
     case car, moto, miniBus, bus
         
     var feeForType: Int {
-//        Switch is used for flow control because it evaluates each case of the enum.
         switch self{
         case .car: return 20
         case .moto: return 15
