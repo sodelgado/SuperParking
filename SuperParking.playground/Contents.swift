be2c0f7717fe86423698ebf61dfac1078bb79aa8
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-03-10 15:42:18 -0300

    Added comments
    For struct and properties, also added response for Exercise 1

--- SuperParking.playground/Contents.swift
+++ SuperParking.playground/Contents.swift
@@ -4,18 +4,24 @@ protocol Parkable {
     var plate: String { get }
 }
 
+
+// MARK: struct Parking - represents the parking
 struct Parking {
+    // Property vehicles - it stores parked vehicles
+    // MARK: Aswer exercise 1:
+    // Why is it defined as a set? -> because it doesn't allow repeating values
+
     var vehicles: Set<Vehicle> = []
 }
 
+// MARK: Struct Vehicle - implements Hashable protocol, used to uniquely identify elements.
 struct Vehicle: Parkable, Hashable {
-    let plate: String
+    let plate: String // Corresponds to the license plate of the vehicle.
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(plate)
     }
-    
     static func ==(lhs: Vehicle, rhs: Vehicle) -> Bool {
-        return lhs.plate == rhs.plate
+            return lhs.plate == rhs.plate
+        }
     }
-}
