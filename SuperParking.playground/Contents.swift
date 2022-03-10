4f90e77ea148e54c00e3b65806664218cc6e2931
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-03-10 19:31:28 -0300

    Exercise 4
    Added  parkedTime computed property.
    Added mutating func checkInVehicle into Parking struct.
    Added code function checks.

--- SuperParking.playground/Contents.swift
+++ SuperParking.playground/Contents.swift
@@ -13,7 +13,7 @@ protocol Parkable {
     var type: VehicleType { get  }
     var checkInTime: Date { get }
     var discountCard: String? { get }
-    var parkedTime: Calendar { get }
+    var parkedTime: Int { get }
 }
 
 
@@ -23,17 +23,23 @@ protocol Parkable {
 // Why is it defined as a set? -> because it doesn't allow repeating values
 struct Parking {
     var vehicles: Set<Vehicle> = []
+    
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
+
+    }
 }
 
 // MARK: Struct Vehicle - implements Hashable protocol, used to uniquely identify elements.
 //    MARK: Answer exercise 2.2:
 //    type is a constant because Vehicle can't changes his VehicleType
 struct Vehicle: Parkable, Hashable {
-    var parkedTime: Calendar
-    var discountCard: String?
-    var checkInTime: Date
-    let type: VehicleType
     let plate: String // Corresponds to the license plate of the vehicle.
+    let type: VehicleType
+    var checkInTime: Date
+    var discountCard: String?
+    
+    var parkedTime: Int {
+        Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0 }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(plate)
@@ -57,3 +63,36 @@ enum VehicleType {
         }
     }
 }
+
+// MARK: Instances are created and added to the Parking set
+var superParking = Parking()
+let car = Vehicle(plate: "AA111AA", type: VehicleType.car,
+checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+let moto = Vehicle(plate: "B222BBB", type:
+VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let miniBus = Vehicle(plate: "CC333CC", type:
+VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let bus = Vehicle(plate: "DD444DD", type: VehicleType.bus,
+checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
+superParking.vehicles.insert(car)
+superParking.vehicles.insert(moto)
+superParking.vehicles.insert(miniBus)
+superParking.vehicles.insert(bus)
+ 
+// MARK: verify that the inserts were successful for console
+//print(superParking.vehicles.insert(car).inserted)  // true
+//print(superParking.vehicles.insert(moto).inserted)  // true
+//print(superParking.vehicles.insert(miniBus).inserted)// true
+//print(superParking.vehicles.insert(bus).inserted)  // true
+
+//MARK: verify the values are not duplicated
+//let car = Vehicle(plate: "AA111AA", type: VehicleType.car,
+//checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+//let car2 = Vehicle(plate: "AA111AA", type: VehicleType.car,
+//checkInTime: Date(), discountCard: "DISCOUNT_CARD_003")
+//print(superParking.vehicles.insert(car).inserted)  // true
+//print(superParking.vehicles.insert(car2).inserted)  // false
+
+// MARK: verify that can be removed
+//superParking.vehicles.remove(moto)
+
