cab848ebb48376191d1c49ab343ede7f00ee5550
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-03-14 15:09:00 -0300

    added comments

--- SuperParking.playground/Contents.swift
+++ SuperParking.playground/Contents.swift
@@ -1,32 +1,21 @@
 import UIKit
 
-//MARK: protocol Parkable
-
-// MARK: Answer exercise 3.1:
-// As Vehicle implements Parkable protocol, the properties must be added in both
-// MARK: Answer exercise 3.2:
-// All data types can be optional
-// MARK: Answer exercise 4:
-// Computed properties have the ability to be computed each time they are queried
+// MARK: protocol Parkable
 protocol Parkable {
     var plate: String { get }
-    var type: VehicleType { get  }
+    var type: VehicleType { get }
     var checkInTime: Date { get }
     var discountCard: String? { get }
     var parkedTime: Int { get }
 }
 
-
-// MARK: struct Parking - represents the parking
-// Property vehicles - it stores parked vehicles
-// MARK: Aswer exercise 1:
-// Why is it defined as a set? -> because it doesn't allow repeating values
+// MARK: struct Parking
 struct Parking {
-    var vehicles: Set<Vehicle> = []
-    let maxVehicles: Int = 20
+    var vehicles: Set<Vehicle> = [] // Property vehicles - it stores parked vehicles
+    let maxVehicles: Int = 20       // Determines the maximum number of vehicles in the parking lot
     
-//    MARK: func checkInVehicle - validates the maximum number of vehicles in the parking lot and that the license plate is not duplicated
-    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void){
+    // MARK: func checkInVehicle
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
         guard vehicles.count < maxVehicles, vehicles.insert(vehicle).inserted else {
             onFinish(false)
             print("Sorry, the check-in failed")
@@ -36,29 +25,27 @@ struct Parking {
         print("Welcome to SuperParking!")
     }
     
-// MARK: func checkOut - find vehicle plate and remove it when check out 
-    mutating func checkOut(_ plate: String, onSuccess: (Int) -> (), onError: () -> ()) {
+    // MARK: Answer exercise 7
+    // The checkout function must be mutating because being inside a struct it is assumed that it is immutable by default
+    // MARK: func checkOutVehicle
+    // Find vehicle plate and remove it when check out
+    mutating func checkOutVehicle(_ plate: String, onSuccess: (Int) -> (), onError: () -> ()) {
         guard let vehicle = vehicles.first(where: { $0.plate == plate }) else {
-            onError
-            print("Plate not found")
+            onError()
             return
         }
-        superParking.vehicles.remove(vehicle)
+        vehicles.remove(vehicle)
         onSuccess(30)
     }
-    
 }
 
-// MARK: Struct Vehicle - implements Hashable protocol, used to uniquely identify elements.
-//    MARK: Answer exercise 2.2:
-//    type is a constant because Vehicle can't changes his VehicleType
+// MARK: struct Vehicle
 struct Vehicle: Parkable, Hashable {
-    let plate: String // Corresponds to the license plate of the vehicle.
-    let type: VehicleType
-    var checkInTime: Date
-    var discountCard: String?
-    
-    var parkedTime: Int {
+    let plate: String           // Corresponds to the license plate of the vehicle.
+    let type: VehicleType       // Corresponds to the type of the vehicle
+    var checkInTime: Date       // Corresponds to the date of entry of the vehicle
+    var discountCard: String?   // Corresponds to an optional vehicle discount card
+    var parkedTime: Int {       // corresponds to the total in minutes of the stay of a vehicle
         Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0 }
     
     func hash(into hasher: inout Hasher) {
@@ -69,9 +56,7 @@ struct Vehicle: Parkable, Hashable {
     }
 }
 
-
-// MARK: enum VehicleType - determines vehicle type and initial fee
-//        Switch is used for flow control because it evaluates each case of the enum.
+// MARK: enum VehicleType
 enum VehicleType {
     case car, moto, miniBus, bus
     
@@ -85,10 +70,10 @@ enum VehicleType {
     }
 }
 
-// MARK: Instances are created and added to the Parking set
+// MARK: Parking set
 var superParking = Parking()
 
-//MARK: Entry of vehicles for checkIn validation
+// MARK: Entry of vehicles for check-In validation
 let vehicle1 = Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
 let vehicle2 = Vehicle(plate: "B222BBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
 let vehicle3 = Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
@@ -109,17 +94,15 @@ let vehicle17 = Vehicle(plate: "YY453UU", type: VehicleType.miniBus, checkInTime
 let vehicle18 = Vehicle(plate: "OO111QQ", type: VehicleType.bus, checkInTime: Date(), discountCard: nil)
 let vehicle19 = Vehicle(plate: "RR666EE", type: VehicleType.moto, checkInTime: Date(), discountCard: "DISCOUNT_CARD_009")
 let vehicle20 = Vehicle(plate: "RR666EE", type: VehicleType.car, checkInTime: Date(), discountCard: nil)
-
 let vehicle21 = Vehicle(plate: "GGG11MA", type: VehicleType.bus, checkInTime: Date(), discountCard: nil)
 let vehicle22 = Vehicle(plate: "QQ77DE", type: VehicleType.miniBus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_010")
 
-//MARK: Insertion of vehicles in the array vehicles
-let vehicles = [ vehicle1, vehicle2, vehicle3, vehicle4, vehicle5, vehicle6, vehicle7, vehicle8, vehicle9, vehicle10, vehicle11, vehicle12, vehicle13, vehicle14, vehicle15, vehicle16, vehicle17, vehicle18, vehicle19, vehicle20, vehicle21, vehicle22]
+// MARK: Insertion of vehicles in the array vehicles
+let vehicles = [vehicle1, vehicle2, vehicle3, vehicle4, vehicle5, vehicle6, vehicle7, vehicle8, vehicle9, vehicle10, vehicle11, vehicle12, vehicle13, vehicle14, vehicle15, vehicle16, vehicle17, vehicle18, vehicle19, vehicle20, vehicle21, vehicle22]
 
-//MARK: Vehicles CheckIn
+// MARK: Vehicles Check-In
 for vehicle in vehicles {
-  superParking.checkInVehicle(vehicle) { successfulEntry in
-    successfulEntry ? true:false }
+  superParking.checkInVehicle(vehicle) { successfulEntry in successfulEntry ? true:false }
 }
 
 
